f02f9cb7149eaa1e2e783281b9b65b2516acd0d0
Neha S <neha.s@alacergroup.com>
2023-03-06 12:45:16 +1100

    added validations

--- InterviewPreezieAPI/UsersDTO.cs
+++ InterviewPreezieAPI/UsersDTO.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InterviewPreezieAPI
 {
     public class Users
     {
-        
-  //      public long ID { get; set; }
+
+        //      public long ID { get; set; }
+        [Required(ErrorMessage = "EmailId is required")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "DisplayName is required")]
         public string DisplayName { get; set; }
     }
 
